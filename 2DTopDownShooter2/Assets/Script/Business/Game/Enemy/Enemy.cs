0953a8b950d6f37269f40d852cb88dd47afec8a3
zsteoh2 <zsteoh2@gmail.com>
2023-07-30 00:42:54 +0800

    敌人追踪

--- 2DTopDownShooter2/Assets/Script/Business/Game/Enemy/Enemy.cs
+++ 2DTopDownShooter2/Assets/Script/Business/Game/Enemy/Enemy.cs
@@ -9,14 +9,47 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] float health, maxHealth = 3f;
 
+    [SerializeField] float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
 
     private void Start()
     {
         health = maxHealth;
+        target = GameObject.Find("Player").transform;
     }
+
+    private void Update()
+    {
+        if(target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
+
+
     public void TakeDamage(float damageAmount)
     {
-        health -= damageAmount; //敌人生命进入倒数，直至0 = 敌人已死亡
+        health -= damageAmount; //敌人生命进入倒数，直至0 = 敌人死亡
 
         if(health <= 0)
         {
