a4cc4cac4628931964bd4f57a3e5939c6b6b5988
zsteoh2 <zsteoh2@gmail.com>
2023-08-19 12:57:51 +0800

    Added Animation and skin for player

--- 2DTopDownShooter2/Assets/Script/Business/Game/Player/PlayerController.cs
+++ 2DTopDownShooter2/Assets/Script/Business/Game/Player/PlayerController.cs
@@ -10,11 +10,18 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     public Weapon weapon;
+    public Animator playerAnimator;
+    public bool isWalking;
 
     private Vector2 moveDirection;
 
     private Vector2 mousePosition;
 
+
+    private void Start()
+    {
+        playerAnimator = transform.Find("PlayerSprite").GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -34,6 +41,9 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        //Check if the player is walking
+        isWalking = (Mathf.Abs(moveX) > 0.1f || Mathf.Abs(moveY) > 0.1f);
+
         if(Input.GetMouseButtonDown(0))
         {
             weapon.Fire();
@@ -47,6 +57,10 @@ public class PlayerController : MonoBehaviour
 
         moveDirection = new Vector2(moveX, moveY).normalized;
         mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        //Update the "isWalking" parameter in the Animator
+        playerAnimator.SetBool("isWalking", isWalking);
+
     }
 
   
